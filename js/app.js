d9c0fba9ee5f0ff7a66b066d58084025104ad97f
Fernando Pinedo <fernando.pinedo@outlook.com>
2018-01-02 02:13:25 -0600

    Resuelve problema de iniciacion

--- js/app.js
+++ js/app.js
@@ -65,11 +65,7 @@ var Calculadora = {
     this.display.textContent = text;
   },
 
-  effect : function(button)
-  {
-    button.style.padding = "1px";
-    setTimeout(function(){button.style.padding = "0px";}, 50);
-  },
+  
 
   btnNumber : function(button)
   {
@@ -134,43 +130,44 @@ var Calculadora = {
 
   init : function()
   {
-    for (var i = 0; i < 19; i++)
-    {
-      if (i!=2)
-      {
-        if (i!=3 && i!=7 && i!=11 && i!=17 && i!=18)
-        {
-          Calculadora.buttons[i].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[i])}
-        }
-        else
-        {
-          Calculadora.buttons[i].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[i])}
-        }
-      }
-    }
-    Calculadora.buttons[2].onclick = function(){ Calculadora.effect(Calculadora.buttons[2])}
+    // no funciona
+    // for (var i = 0; i < 19; i++)
+    // {
+    //   if (i!=2)
+    //   {
+    //     if (i!=3 && i!=7 && i!=11 && i!=17 && i!=18)
+    //     {
+    //       Calculadora.buttons[i].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[i])}
+    //     }
+    //     else
+    //     {
+    //       Calculadora.buttons[i].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[i])}
+    //     }
+    //   }
+    // }
+    // Calculadora.buttons[2].onclick = function(){ Calculadora.effect(Calculadora.buttons[2])}
 
-    // this.buttons[0].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[0])}
-    // this.buttons[1].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[1])}
-    // this.buttons[4].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[4])}
-    // this.buttons[5].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[5])}
-    // this.buttons[6].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[6])}
-    // this.buttons[8].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[8])}
-    // this.buttons[9].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[9])}
-    // this.buttons[10].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[10])}
-    // this.buttons[12].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[12])}
-    // this.buttons[13].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[13])}
-    // this.buttons[14].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[14])}
-    // this.buttons[15].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[15])}
-    // this.buttons[16].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[16])}
-    //
-    // this.buttons[3].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[3])}  // dividido
-    // this.buttons[7].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[7])}  // por
-    // this.buttons[11].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[11])} // resta
-    // this.buttons[17].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[17])} // igual
-    // this.buttons[18].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[18])} // mas
-    //
-    // this.buttons[2].onclick = function(){ Calculadora.effect(Calculadora.buttons[2])} // raiz
+    this.buttons[0].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[0])}
+    this.buttons[1].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[1])}
+    this.buttons[4].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[4])}
+    this.buttons[5].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[5])}
+    this.buttons[6].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[6])}
+    this.buttons[8].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[8])}
+    this.buttons[9].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[9])}
+    this.buttons[10].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[10])}
+    this.buttons[12].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[12])}
+    this.buttons[13].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[13])}
+    this.buttons[14].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[14])}
+    this.buttons[15].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[15])}
+    this.buttons[16].onclick = function(){ Calculadora.btnNumber(Calculadora.buttons[16])}
+
+    this.buttons[3].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[3])}  // dividido
+    this.buttons[7].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[7])}  // por
+    this.buttons[11].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[11])} // resta
+    this.buttons[17].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[17])} // igual
+    this.buttons[18].onclick = function(){ Calculadora.btnOperator(Calculadora.buttons[18])} // mas
+
+    this.buttons[2].onclick = function(){ Calculadora.effect(Calculadora.buttons[2])} // raiz
 
     console.log("INICIADO");
   }
